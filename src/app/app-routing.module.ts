716a188255ebb0dfd39cd90bada002b73b9537e2
Machoka Daisy <machokadaisy25@gmail.com>
2020-11-19 08:27:13 +0300

    add routing paths for home and home-detail components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HomeDetailComponent } from './home-detail/home-detail.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'houses', component: HomeComponent},
+  {path: 'houses/:id', component: HomeDetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
