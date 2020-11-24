84cb5df4c0ddd2af324fe11fb9cd26a961ac58e8
MichaelOmondi <omondimike11@gmail.com>
2020-11-24 17:43:40 +0300

    merge the conflicting files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,52 +6,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HomeDetailComponent } from './home-detail/home-detail.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { LandingComponent } from './landing/landing.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HouseFormComponent } from './house-form/house-form.component';
-import { HouseListComponent } from './house-list/house-list.component';
 import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HomeDetailComponent,
-    LandingComponent,
-    NavbarComponent,
-    HouseFormComponent,
-    HouseListComponent,
-    ReviewComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    HttpClientModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-    FormsModule,
-    ReactiveFormsModule
-    
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
